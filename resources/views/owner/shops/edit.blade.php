d5f9635a14bfae30056daf890b046f3ff96603d7
Brave-lab-Yuki <hiroo914.28@gmail.com>
2023-05-01 14:29:48 +0900

    product create

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -32,22 +32,25 @@
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                     <div class="w-32">
-                                        <x-thumbnail :filename="$shop->filename" type="shops"/>
+                                        <x-thumbnail :filename="$shop->filename" type="shops" />
                                     </div>
                                 </div>
                             </div>
                             <label for="image" class="leading-7 text-sm text-gray-600 flex justify-center">画像</label>
                             <div class="relative flex justify-center">
-                            <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg"
-                                class="w-1/2 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input type="file" id="image" name="image"
+                                    accept="image/png,image/jpeg,image/jpg"
+                                    class="w-1/2 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
-                                <div class="p-2 w-1/2 mx-auto">
-                                    <div class="relative flex justify-around">
-                                        <div><input type="radio" name="is_selling" value="1" class="mr-2" @if ($shop->is_selling === 1) { checked } @endif>販売中</div>
-                                        <div><input type="radio" name="is_selling" value="0" class="mr-2" @if ($shop->is_selling === 0) { checked } @endif>停止中</div>
-                                    </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="is_selling" value="1" class="mr-2"
+                                            @if ($shop->is_selling === 1) { checked } @endif>販売中</div>
+                                    <div><input type="radio" name="is_selling" value="0" class="mr-2"
+                                            @if ($shop->is_selling === 0) { checked } @endif>停止中</div>
                                 </div>
-    
+                            </div>
+
                         </div>
                 </div>
             </div>
