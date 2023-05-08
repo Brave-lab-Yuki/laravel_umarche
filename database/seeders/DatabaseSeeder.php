5ac6d74a5044cdc105a164bd73b36b9ec36b6c82
Brave-lab-Yuki <hiroo914.28@gmail.com>
2023-05-08 14:53:49 +0900

    route,itemcontroller,index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class
+            UserSeeder::class,
+
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Owner\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\User\ItemController;
+
 
 
 
@@ -21,10 +23,13 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('user.welcome');
 });
+Route::middleware('auth:users')->group(function () {
+    Route::get('/', [ItemController::class,'index'])->name('items.index');
+});
 
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
 
     Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
     Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
