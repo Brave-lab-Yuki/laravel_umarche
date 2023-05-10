13a348a3932e7a43c66af4c17f00055632beb0b2
Brave-lab-Yuki <hiroo914.28@gmail.com>
2023-05-10 15:04:56 +0900

    cart index

--- routes/web.php
+++ routes/web.php
@@ -25,22 +25,21 @@ Route::get('/', function () {
     return view('user.welcome');
 });
 Route::middleware('auth:users')->group(function () {
-    Route::get('/', [ItemController::class,'index'])->name('items.index');
-    Route::get('show/{item}',[ItemController::class, 'show'])->name('items.show');
-
+    Route::get('/', [ItemController::class, 'index'])->name('items.index');
+    Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-Route::prefix('cart')->
-middleware('auth:users')->group(function () {
-    Route::post('add', [CartController::class,'add'])->name('cart.add');
-});
+Route::prefix('cart')->middleware('auth:users')->group(function () {
+    Route::get('/',[CartController::class,'index'])->name('cart.index');
+        Route::post('add', [CartController::class, 'add'])->name('cart.add');
+    });
 // Route::get('/dashboard', function () {
 //     return view('user.dashboard');
 // })->middleware(['auth:users'])->name('dashboard');
 
-    Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
-    Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
-    Route::get('/servicecontainer', [LifeCycleTestController::class, 'showServiceContainerTest']);
+Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainer', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
